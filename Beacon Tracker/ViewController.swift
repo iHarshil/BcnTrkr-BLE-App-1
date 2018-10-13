cecfd5a0c7e6352314b78841f2ef01e00a6ad75c
macbook <macbook@macbooks-macbook-pro.local>
2018-10-13 12:24:39 +0530

    Added ViewWillAppear

--- Beacon Tracker/ViewController.swift
+++ Beacon Tracker/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+    }
 }
 
