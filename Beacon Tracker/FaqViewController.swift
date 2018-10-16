4ec51f8170ce0ef0fc1df19f77f0237167266fb7
macbook <macbook@macbooks-MacBook-Pro.local>
2018-10-16 19:10:12 +0530

    Fetch Details for particular Device and added Map to current location with other screen designs

---
+++ Beacon Tracker/FaqViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FaqViewController.swift
+//  Beacon Tracker
+//
+//  Created by macbook on 10/16/18.
+//  Copyright © 2018 Teksun. All rights reserved.
+//
+
+import UIKit
+
+class FaqViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
